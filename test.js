6f5a14fdf9b50ba88e9a16aac2133f5abb1335fd
Vinit Kumar Singh <vinitksingh@live.com>
2015-09-22 11:38:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ describe('User visits Hello World page', function() {
   const browser = new Browser();
 
   before(function(done) {
-    browser.visit('http://<your-ip>:8081/<your-path>/', done);
+    browser.visit('http://146.148.43.172:8081/HelloWorld/', done);
   });
 
   describe('submits form', function() {
